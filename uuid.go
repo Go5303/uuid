a64cacc95d9652d2a18a21312129ff2c31c99cea
张海 <zhanghai@woda.ink>
2024-07-18 08:59:21 +0800

    -fix

--- uuid.go
+++ uuid.go
@@ -7,11 +7,11 @@ import (
 
 const StaticStr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
 func Uuid() string {
 	var str string
 	strLen := len(StaticStr)
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
 	for i := 0; i <= 100; i++ {
 		number := r.Intn(strLen)
 		str += StaticStr[number : number+1]
